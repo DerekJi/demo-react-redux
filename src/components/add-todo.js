4396462e14c09f0d3b078525ddf372bfd6b78655
Derek Ji <syfool@gmail.com>
2020-04-22 16:41:51 +0930

    Rewrite sumbit() as an internal method

--- src/components/add-todo.js
+++ src/components/add-todo.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-
+import { addTodo } from '../actions';
 
 class AddTodo extends Component {
-  constructor({ submit }) {
+  constructor({ dispatch }) {
     super();
 
     this.state = { value: ''};
 
   }
 
-  inputChange(event) {
+  onInputChange(event) {
     this.setState({
       value: event.target.value
     });
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+    if (!this.state.value.trim()) {
+      return;
+    }
+    this.props.dispatch(addTodo(this.state.value));
+    this.setState({ value: ''});
+  }
+
   render() {
     return (
       <div>
-        <form onSubmit={(e) => this.props.submit(e, this.state.value)}>
-          <input value={this.state.value} onChange={this.inputChange.bind(this)} />
+        <form onSubmit={(e) => this.onSubmit(e)}>
+          <input value={this.state.value} onChange={this.onInputChange.bind(this)} />
           <button type="submit">Add Todo</button>
         </form>
       </div>
--- src/containers/add-todo.js
+++ src/containers/add-todo.js
@@ -1,16 +1,4 @@
 import { connect } from 'react-redux';
 import AddTodo from '../components/add-todo';
-import { addTodo } from '../actions';
 
-const mapDispatchToProps = dispatch => ({
-  submit: (e, value) => {
-    e.preventDefault();
-    if (!value.trim()) {
-      return;
-    }
-    dispatch(addTodo(value));
-    value = '';
-  }
-});
-
-export default connect(null, mapDispatchToProps)(AddTodo);
+export default connect()(AddTodo);
