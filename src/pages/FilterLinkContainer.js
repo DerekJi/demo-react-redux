09079b132932774a0fee7a8a10850b031e06caba
Derek Ji <syfool@gmail.com>
2020-04-25 17:31:44 +0930

    Mark the selected filter button

--- src/pages/FilterLinkContainer.js
+++ src/pages/FilterLinkContainer.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { filter as filterActionCreator } from '../store/filter/filter.actions';
+import './FilterLinkContainer.scss';
 
-const FilterLink = ({filter, children, onClick}) => (
-  <button onClick={onClick}>{children}</button>
+const FilterLink = ({filter, children, onClick, active}) => (
+  <button onClick={onClick} className={active ? 'active' : ''}>{children}</button>
 );
+
+const mapStatesToProps = (states, ownProps) => {
+  return {
+    active: ownProps.filter === states.filter
+  };
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onClick: () => dispatch(filterActionCreator(ownProps.filter))
 });
 
-export default connect(null, mapDispatchToProps)(FilterLink);
+export default connect(mapStatesToProps, mapDispatchToProps)(FilterLink);
